a254a214f13a4fed4514b8880de67b1cd86f2e25
Marcelo Coviello <markkcoviello@gmail.com>
2025-10-31 14:34:35 -0700

    Ajusta API de PDF y agrega config de Vercel2

--- src/app/reporte/[id]/page.tsx
+++ src/app/reporte/[id]/page.tsx
@@ -140,7 +140,10 @@ export default async function ReportePage({ params }: { params: { id: string } }
             </div>
           </div>
         </footer>
+        {/* Espacio para el salto de página */}
+        <div className={styles.pageBreakSpacer}></div>
       </div>
     </main>
+     
   );
 }
